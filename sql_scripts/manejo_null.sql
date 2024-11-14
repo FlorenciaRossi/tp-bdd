4f13d21cc6116cd76eb3d0fcdc7fcce31cd87651
FlorenciaRossi <flrossi@estudiantes.unsam.edu.ar>
2024-11-14 19:41:03 -0300

    add manejo de null

---
+++ sql_scripts/manejo_null.sql
@@ -0,0 +1,31 @@
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE alumnos
+SET dni = NULL
+WHERE dni = 0;
+
+UPDATE alumnos
+SET nombre = NULL
+WHERE nombre = "";
+
+UPDATE alumnos
+SET apellido = NULL
+WHERE apellido = "";
+
+UPDATE alumnos
+SET email = NULL
+WHERE email = "";
+
+UPDATE alumnos
+SET ubicacion_id = NULL
+WHERE ubicacion_id = 0;
+
+UPDATE alumnos
+SET recomendacion_multimedia = NULL
+WHERE recomendacion_multimedia = "";
+
+select * from alumnos;
+
+
+
+
